f386af6eeefef0dff6750b033880876d2dab45dc
Azizb283 <azizbhavanagarwala@gmail.com>
2023-05-01 09:49:48 +0530

    React Hooks

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -3,19 +3,23 @@ import CompA from "./component/CompA";
 import ContextA from "./ContextA";
 import Ref from "./component/Ref";
 import UseRef from "./component/UseRef";
+import Calc from "./component/Calc";
+import './styles.css'
 
 const name = createContext();
 
 function App() {
   
   return <>
-  <name.Provider value = {"Aziz"}>
+  {/* <name.Provider value = {"Aziz"}>
     <h1>useContext Hook</h1>
     <CompA />
     <ContextA />
     <Ref />
     <UseRef />
-  </name.Provider>
+  </name.Provider> */}
+
+  <Calc />
   </>;
 }
 
---
+++ reacthooks/src/component/Calc.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CompA from './CompA'
+import ContextA from '../ContextA'
+import Ref from './Ref'
+import UseRef from './UseRef'
+
+
+function Calc() {
+  return (
+    <>
+    <CompA />
+    <ContextA />
+    <Ref />
+    <UseRef />
+    </>
+  )
+}
+
+export default Calc
